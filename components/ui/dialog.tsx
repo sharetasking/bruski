bd97d68879191df946def99f9914d34fbbbd21ac
Graphify Inc <help@graphify.ai>
2024-01-18 16:58:31 -0500

    updates

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -11,9 +11,10 @@ const Dialog = DialogPrimitive.Root
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = ({
+  className,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn()} {...props} />
+  <DialogPrimitive.Portal className={cn(className)} {...props} />
 )
 DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
