cfcb9b6887150282989f8b120bde705847471b47
Graphify Inc <help@graphify.ai>
2024-01-22 21:30:59 -0500

    terms and privacy pages

---
+++ app/(root)/(routes)/privacy/page.tsx
@@ -0,0 +1,62 @@
+const PrivacyPage = () => {
+  return ( 
+    <div className="p-8 pb-40 max-w-2xl">
+      <h1>Bruski<br/>Privacy Policy</h1>
+      <h4>Last Updated: Jan 22, 2024</h4>
+
+<b>Welcome to Bruski!</b>
+
+<p>Your privacy is important to us.</p>
+
+<p>Bruski (&quot;we,&quot; &quot;our,&quot; or &quot;us&quot;) is committed to protecting the privacy of your personal information. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and use our services (collectively, &quot;Services&quot;).</p>
+
+<h3>1. Information We Collect</h3>
+<p>a. Personal Information: We collect personal information that you voluntarily provide to us when registering for the Services, such as your name, email address, and username.</p>
+
+<p>b. Usage and Device Information: When you access our Services, we may automatically collect information about your device and usage of the Services, including IP address, browser type, and activity logs.</p>
+
+<p>c. Cookies and Tracking Technologies: We use cookies and similar tracking technologies to track activity on our Services and store certain information.</p>
+
+<h3>2. How We Use Your Information</h3>
+<p>We use your information for various purposes, including to:</p>
+
+<p>Provide and manage our Services.</p>
+<p>Improve your user experience.</p>
+<p>Communicate with you, including for customer service, updates, and promotional purposes.</p>
+<p>Monitor and analyze usage and trends to improve your experience with the Services.</p>
+
+<h3>3. Sharing Your Information</h3>
+<p>We may share your information as follows:</p>
+
+<p>Service Providers: We may share your information with third parties that perform services for us or on our behalf, including payment processing, data analysis, email delivery, hosting services, and customer service.</p>
+
+Legal Obligations: We may share your information if required to do so by law or in response to valid requests by public authorities (e.g., a court or a government agency).
+
+Marketing and Advertising: We may share your information with third-party advertisers and marketers for promotional purposes, in compliance with legal obligations and your consent where required.
+
+<h3>4. Your Privacy Rights</h3>
+Depending on your location, you may have certain rights regarding your personal information, such as the right to access, update, or delete the information we have on you.
+
+<h3>5. Security of Your Information</h3>
+We use administrative, technical, and physical security measures to protect your personal information from unauthorized access, use, or disclosure.
+
+<h3>6. International Data Transfers</h3>
+Your information may be transferred to and maintained on computers located outside of your state, province, country, or other governmental jurisdiction where the data protection laws may differ from those of your jurisdiction.
+
+<h3>7. Children&apos;s Privacy</h3>
+Our Services are not intended for use by children under the age of 13. We do not knowingly collect personally identifiable information from children under 13.
+
+<h3>8. Changes to This Privacy Policy</h3>
+We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+
+<h3>9. Contact Us</h3>
+If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at help@bruski.app.
+
+
+
+
+    </div>
+   );
+}
+ 
+export default PrivacyPage;
---
+++ app/(root)/(routes)/terms/page.tsx
@@ -0,0 +1,54 @@
+const TermsPage = () => {
+  return (
+  
+  <div className="p-8 pb-40 max-w-2xl"> 
+
+<h1>Bruski<br/>Terms of Service</h1>
+<h4>Last Updated: Jan 22, 2024</h4>
+
+<p>Welcome to Bruski!</p>
+<p>These Terms of Service (&quot;Terms&quot;) govern your access to and use of Bruski&apos;s website, products, and services (&quot;Services&quot;). Please read these Terms carefully, and contact us if you have any questions. By accessing or using our Services, you agree to be bound by these Terms and our Privacy Policy.
+</p>
+
+
+<h3>1. Using Bruski</h3>
+<h4>a. Who can use Bruski</h4>
+You may use our Services only if you can form a binding contract with Bruski, and only in compliance with these Terms and all applicable laws. When you create your Bruski account, you must provide us with accurate and complete information. Any use or access by anyone under the age of 13 is prohibited.
+
+<h4>b. Our license to you</h4>
+Subject to these Terms and our policies (including our Community Guidelines), we grant you a limited, non-exclusive, non-transferable, and revocable license to use our Services.
+
+<h3>2. Your Content</h3>
+<h4>a. Posting content</h4>
+Bruski allows you to post content, including photos, comments, links, and other materials. Anything that you post or otherwise make available on our Services is referred to as &quot;User Content.&quot; You retain all rights in, and are solely responsible for, the User Content you post to Bruski.
+
+<h4>b. How Bruski and other users can use your content</h4>
+You grant Bruski and its users a non-exclusive, royalty-free, transferable, sublicensable, worldwide license to use, store, display, reproduce, save, modify, create derivative works, perform, and distribute your User Content on Bruski solely for the purposes of operating, developing, providing, and using the Bruski Services.
+
+<h4>c. How long we keep your content</h4>
+Following termination or deactivation of your account, or if you remove any User Content from Bruski, we may retain your User Content for a commercially reasonable period of time for backup, archival, or audit purposes. Furthermore, Bruski and its users may retain and continue to use, store, display, reproduce, modify, create derivative works, perform, and distribute any of your User Content that other users have stored or shared through Bruski.
+
+<h3>3. Bruski Rights</h3>
+All rights, title, and interest in and to the Services (excluding User Content) are and will remain the exclusive property of Bruski and its licensors. Nothing in the Terms gives you a right to use the Bruski name or any of the Bruski trademarks, logos, domain names, and other distinctive brand features. Any feedback, comments, or suggestions you may provide regarding Bruski or the Services is entirely voluntary and we will be free to use such feedback, comments, or suggestions as we see fit and without any obligation to you.
+
+<h3>4. Privacy</h3>
+Our Privacy Policy describes how we handle the information you provide to us when you use our Services. You understand that through your use of the Services you consent to the collection and use (as set forth in the Privacy Policy) of this information.
+
+<h3>5. Security</h3>
+We care about the security of our users. While we work to protect the security of your content and account, Bruski cannot guarantee that unauthorized third parties will not be able to defeat our security measures. Please notify us immediately of any compromise or unauthorized use of your account.
+
+<h3>6. Termination</h3>
+Bruski may terminate or suspend this license at any time, with or without cause or notice to you. Upon termination, you continue to be bound by Sections 2 and 3 of these Terms.
+
+<h3>7. Disclaimers</h3>
+Our Services are provided &quot;as is.&quot; Bruski and its suppliers and licensors hereby disclaim all warranties of any kind, express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose, and non-infringement. Neither Bruski nor its suppliers and licensors make any warranty that the Services will be error-free or that access thereto will be continuous or uninterrupted.
+
+<h3>8. Limitation of Liability</h3>
+In no event will Bruski, or its suppliers or licensors, be liable with respect to any subject matter of these terms under any contract, negligence, strict liability, or other legal or equitable theory for: (i) any special, incidental, or consequential damages; (ii) the cost of procurement for substitute products or services; (iii) for interruption of use or loss or corruption of data; or (iv) for any amounts that exceed the fees
+
+
+
+  </div> );
+}
+ 
+export default TermsPage;
